912104792c3aec471868587841e34752fe8ff71f
Agajan Torayev <agajan.torayev@iais.fraunhofer.de>
2019-03-20 16:24:53 +0100

    bug fix

--- src/main/cpp/starspace_wrapper.cc
+++ src/main/cpp/starspace_wrapper.cc
@@ -8,12 +8,6 @@ namespace StarSpaceWrapper {
 
     using namespace starspace;
 
-    StarSpaceApi::StarSpaceApi() {
-        // HACK: A trick to get access to FastText's private members.
-        // Reference: http://stackoverflow.com/a/8282638
-        privateMembers = (StarSpacePrivateMembers*) &starSpace;
-    }
-
     void StarSpaceApi::runCmd(int argc, char **argv) {
         main(argc, argv);
     }
